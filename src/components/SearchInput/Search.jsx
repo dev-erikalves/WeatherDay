3849c15e3d257cfe4cab21c850f735502e81ebd7
Erik Alves <dev.eriksantos@gmail.com>
2023-06-03 16:37:30 -0300

    style(SearchInput): Search input and button styling

--- src/components/SearchInput/Search.jsx
+++ src/components/SearchInput/Search.jsx
@@ -1,10 +1,11 @@
+import SearchIcon from "../../assets/search-icon.svg"
 import styles from "./styles.module.scss";
 
 export default function SearchInput() {
     return (
-        <div>
+        <div className={styles.searchContainer}>
             <input type="search" name="searchInput" id={styles.searchInput} placeholder="Localização" />
-            <button id={styles.SearchBtn}><img src="" alt="" /></button>
+            <button id={styles.searchBtn}><img src={SearchIcon} alt="" /></button>
         </div>    
     )
 }
