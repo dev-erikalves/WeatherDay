4de87558ef94886438da316f9aca7f5802647b99
Erik Alves <dev.eriksantos@gmail.com>
2023-06-03 11:41:35 -0300

    style(styles/): Added mixins and variables

--- src/components/SearchInput/Search.jsx
+++ src/components/SearchInput/Search.jsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.scss";
 export default function SearchInput() {
     return (
         <div>
-            <input type="search" name="searchInput" id="searchInput" placeholder="Localização" />
+            <input type="search" name="searchInput" id={styles.searchInput} placeholder="Localização" />
             <button><img src="" alt="" /></button>
         </div>    
     )
