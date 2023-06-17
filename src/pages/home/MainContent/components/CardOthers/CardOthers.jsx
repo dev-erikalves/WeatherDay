e4ad867b75fad7bde8bfa165d26c9f2adacd9789
Erik Alves <dev.eriksantos@gmail.com>
2023-06-17 20:28:23 -0300

    feat: import styles

--- src/pages/home/MainContent/components/CardOthers/CardOthers.jsx
+++ src/pages/home/MainContent/components/CardOthers/CardOthers.jsx
@@ -4,10 +4,12 @@ import globalIcon from "../../../../../assets/global-icon.gif"
 import visibilityIcon from "../../../../../assets/visibility-icon.gif"
 import pressureIcon from "../../../../../assets/pressure-icon.gif"
 import cloudsIcon from "../../../../../assets/clouds-icon.gif"
+import styles from "./styles.module.scss"
 
 export default function CardOthers({ country, visibility, sea_level, clouds }) {
     return (
         <Card>
+            <h3 className={styles.title}>Outras</h3>
             <Subtitle name='País' weatherInfo={country} icon={globalIcon} />
             <Subtitle name='Visibilidade' weatherInfo={visibility} metric='Km' icon={visibilityIcon} />
             <Subtitle name='Pressão' weatherInfo={sea_level} metric=' hPa' icon={pressureIcon} />
