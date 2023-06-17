d8257ac6e210531048a7a34bffd7d077eaa03ae1
Erik Alves <dev.eriksantos@gmail.com>
2023-06-17 19:22:37 -0300

    feat: include timing, icon and name

--- src/pages/home/Header/Header.jsx
+++ src/pages/home/Header/Header.jsx
@@ -1,16 +1,36 @@
+import { useEffect } from "react";
+import logoHeader from "../../../assets/logo-header.gif"
 import styles from "./styles.module.scss";
 
-export default function Header(){
-    return(
+export default function Header() {
+    useEffect(() => {
+        const timeElement = document.getElementById('time');
+    
+        function updateTime() {
+          const now = new Date();
+    
+          const seconds = ('0' + now.getSeconds()).slice(-2);
+    
+          timeElement.textContent = `${now.getHours()}:${now.getMinutes()}:${seconds}`;
+        }
+    
+        updateTime();
+    
+        const intervalId = setInterval(updateTime, 1000);
+    
+        return () => {
+          clearInterval(intervalId);
+        };
+      }, []);
+    
+    return (
         <header className={styles.header}>
             <div>
-                <img src="" alt="" className={styles.imgContainer}/>
+                <img src={logoHeader} alt="" />
+                <h2>WeatherDay</h2>
             </div>
-            <nav>
-                <a href=""></a>
-                <a href=""></a>
-                <a href=""></a>
-            </nav>
+            <div id="time">
+            </div>
         </header>
     )
 }
