e7f01519b03a9b88d69da70c7c548d409aa08d69
Erik Alves <dev.eriksantos@gmail.com>
2023-06-17 19:25:32 -0300

    feat: include Card component

--- src/pages/home/MainContent/components/CardMoreInfo/CardMoreInfo.jsx
+++ src/pages/home/MainContent/components/CardMoreInfo/CardMoreInfo.jsx
@@ -3,6 +3,7 @@ import sunriseIcon from "../../../../../assets/sunrise-icon.gif"
 import sunsetIcon from "../../../../../assets/sunset-icon.gif"
 import windIcon from "../../../../../assets/wind-icon.gif"
 import temperatureIcon from "../../../../../assets/temperature-icon.gif"
+import Card from "../../../../../components/Card/Card"
 import styles from "./styles.module.scss"
 
 const formatTime = (epochTime) => {
@@ -13,14 +14,14 @@ const formatTime = (epochTime) => {
 }
 export default function CardMoreInfo({ humidity, speed, sunset, sunrise, feels_like }) {
     return (
-        <>
+        <Card>
             <h3 className={styles.title}>Mais Informações</h3>
             <Subtitle name='Vento' weatherInfo={speed} metric='Km/h' icon={windIcon}/>
             <Subtitle name='Humidade' weatherInfo={humidity} metric='%' icon={humidityIcon}/>
             <Subtitle name='Nascer do Sol' weatherInfo={formatTime(sunrise)} metric='hrs' icon={sunriseIcon}/>
             <Subtitle name='Pôr do Sol' weatherInfo={formatTime(sunset)} metric='hrs'icon={sunsetIcon}/>
             <Subtitle name='Sensação' weatherInfo={feels_like} metric='°C' icon={temperatureIcon}/>
-        </>
+        </Card>
     )
 }
 export const Subtitle = ({ name, weatherInfo, metric, icon }) => {
