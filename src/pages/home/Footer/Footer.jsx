0d0bf2efd45a8a1d20949003bf98a510f63a1ee1
Erik Alves <dev.eriksantos@gmail.com>
2023-06-18 12:13:33 -0300

    feat: Import Card, added content, copy and link

--- src/pages/home/Footer/Footer.jsx
+++ src/pages/home/Footer/Footer.jsx
@@ -1,9 +1,13 @@
+import Card from "../../../components/Card/Card"
 import styles from "./styles.module.scss"
 
-export default function Footer(){
-    return(
+export default function Footer() {
+    return (
         <footer>
-            
-        </footer>    
+            <Card className={styles.cardFooter}>
+                <p>&copy; 2023 - All rights reserveds</p>
+                <p id={styles.devBy}>Developed by <a href="https://www.linkedin.com/in/dev-erikalves/" target="_blank">Erik Alves</a></p>
+            </Card>
+        </footer>
     )
 }
