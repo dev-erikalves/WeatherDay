3718ad521073cc9e1e7542cf092f82a4b16a74f5
dev-erikalves <dev.eriksantos@gmail.com>
2023-07-07 13:21:50 -0300

    refactor(CardMoreInfo.jsx): CardOthers which was previously a separate component is now included here using conditional display

--- src/pages/home/MainContent/components/CardMoreInfo/CardMoreInfo.jsx
+++ src/pages/home/MainContent/components/CardMoreInfo/CardMoreInfo.jsx
@@ -3,6 +3,10 @@ import sunriseIcon from "../../../../../assets/sunrise-icon.gif"
 import sunsetIcon from "../../../../../assets/sunset-icon.gif"
 import windIcon from "../../../../../assets/wind-icon.gif"
 import temperatureIcon from "../../../../../assets/temperature-icon.gif"
+import globalIcon from "../../../../../assets/global-icon.gif"
+import visibilityIcon from "../../../../../assets/visibility-icon.gif"
+import pressureIcon from "../../../../../assets/pressure-icon.gif"
+import cloudsIcon from "../../../../../assets/clouds-icon.gif"
 import Card from "../../../../../components/Card/Card"
 import styles from "./styles.module.scss"
 
@@ -12,17 +16,31 @@ const formatTime = (epochTime) => {
     let minutes = date.getMinutes()
     return `${hours}:${minutes}`
 }
-export default function CardMoreInfo({ humidity, speed, sunset, sunrise, feels_like }) {
-    return (
-        <Card>
-            <h3 className={styles.title}>Mais Informações</h3>
-            <Subtitle name='Vento' weatherInfo={speed} metric='Km/h' icon={windIcon}/>
-            <Subtitle name='Humidade' weatherInfo={humidity} metric='%' icon={humidityIcon}/>
-            <Subtitle name='Nascer do Sol' weatherInfo={formatTime(sunrise)} metric='hrs' icon={sunriseIcon}/>
-            <Subtitle name='Pôr do Sol' weatherInfo={formatTime(sunset)} metric='hrs'icon={sunsetIcon}/>
-            <Subtitle name='Sensação' weatherInfo={feels_like} metric='°C' icon={temperatureIcon}/>
-        </Card>
-    )
+export default function CardMoreInfo(
+    { title, humidity, speed, sunset, sunrise, feels_like, country, visibility, sea_level, clouds }
+) {
+    if (title === 'Mais Informações') {
+        return (
+            <Card>
+                <h3 className={styles.title}>{title}</h3>
+                <Subtitle name='Vento' weatherInfo={speed} metric='Km/h' icon={windIcon} />
+                <Subtitle name='Humidade' weatherInfo={humidity} metric='%' icon={humidityIcon} />
+                <Subtitle name='Nascer do Sol' weatherInfo={formatTime(sunrise)} metric='hrs' icon={sunriseIcon} />
+                <Subtitle name='Pôr do Sol' weatherInfo={formatTime(sunset)} metric='hrs' icon={sunsetIcon} />
+                <Subtitle name='Sensação' weatherInfo={feels_like} metric='°C' icon={temperatureIcon} />
+            </Card>
+        )
+    } else {
+        return (
+            <Card>
+                <h3 className={styles.title}>{title}</h3>
+                <Subtitle name='País' weatherInfo={country} icon={globalIcon} />
+                <Subtitle name='Visibilidade' weatherInfo={visibility} metric='Km' icon={visibilityIcon} />
+                <Subtitle name='Pressão' weatherInfo={sea_level} metric=' hPa' icon={pressureIcon} />
+                <Subtitle name='Nuvens' weatherInfo={clouds} metric='%' icon={cloudsIcon} />
+            </Card>
+        )
+    }
 }
 export const Subtitle = ({ name, weatherInfo, metric, icon }) => {
     return (
@@ -34,4 +52,4 @@ export const Subtitle = ({ name, weatherInfo, metric, icon }) => {
             <span>{weatherInfo}{metric}</span>
         </div>
     )
-} 
+}
